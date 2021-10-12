e00a56f5e4aa9e46e2a0c3c3c877aadcf3503fda
matthias linden <matthias.linden@gmx.de>
2021-10-12 15:20:40 +0200

    Data update, expanded BY by hospitalization and VOC

--- Data/Bundeslaender/BY/fetch.py
+++ Data/Bundeslaender/BY/fetch.py
@@ -9,13 +9,21 @@ sdir = "https://www.lgl.bayern.de/gesundheit/infektionsschutz/infektionskrankhei
 #20210118_faelle_alter_geschlecht.csv"
 
 
-def fetch(d):
+def fetch(d,monday):
     files = os.listdir()
-    for i,ft in enumerate(["_todesfalle","_faelle_alter_geschlecht","_faelle_alter_geschlecht_tote"]):
+    startdates = {"_todesfalle":datetime.date(2020,1,1)}
+    startdates["_faelle_alter_geschlecht"] = datetime.date(2020,1,1)
+    startdates["_faelle_alter_geschlecht_tote"] = datetime.date(2020,1,1)
+    startdates["_krankheitsschwere"] = datetime.date(2021,9,6)
+    startdates["_variantenLK_MW"] = datetime.date(2021,10,11)
+    startdates["_variantenLK"] = datetime.date(2021,5,1)
+    
+    for i,ft in enumerate(startdates.keys()):
         f = d+ft+".csv"
-        if f not in files:# and not (d[:4] == "2020" and i == 0):
-            url = sdir+f
-            wget.download(url)
+        if f not in files:
+            if startdates[ft] <= monday :# and not (d[:4] == "2020" and i == 0):
+                url = sdir+f
+                wget.download(url)
         
 def main():
     today = datetime.date.today()
@@ -23,7 +31,7 @@ def main():
     
     for i in range(40):
         fstr = "%d%02d%02d"%(monday.year,monday.month,monday.day)
-        fetch(fstr)
+        fetch(fstr,monday)
         monday = monday-datetime.timedelta(days=7)
 
 if __name__ == "__main__":
